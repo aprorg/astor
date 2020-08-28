bada3059a1db7cf0ab58215424a30e70b7ed5a3e
bqcuong <bqcuong2212@gmail.com>
2020-08-28 23:35:09 +0700

    update

---
+++ regression_evaluation/openpnp-openpnp-130246850.sh
@@ -0,0 +1,10 @@
+java -cp astor.jar fr.inria.main.evolution.AstorMain \
+-location /Users/cuong/IdeaProjects/jrelifix/BugsDataset \
+-dependencies /Users/cuong/IdeaProjects/jrelifix/BugsDataset/target/dependency \
+-faultfile /Users/cuong/IdeaProjects/jrelifix/SusFiles/PerfectFL/openpnp-openpnp-130246850.txt \
+-mode jgenprog \
+-srcjavafolder /src/main/java \
+-srctestfolder /src/test/java \
+-binjavafolder /target/classes \
+-bintestfolder /target/test-classes \
+-stopfirst true
