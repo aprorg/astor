99dbee84b71e9dfd2681711bddf818d5efecb5b2
bqcuong <bqcuong2212@gmail.com>
2020-08-29 16:37:11 +0700

    update

---
+++ regression_evaluation/Bears-42.sh
@@ -0,0 +1,10 @@
+java -Xmx6g -cp astor.jar fr.inria.main.evolution.AstorMain \
+-location /Users/cuong/IdeaProjects/jrelifix/BugsDataset \
+-dependencies /Users/cuong/IdeaProjects/jrelifix/BugsDataset/target/dependency \
+-faultfile /Users/cuong/IdeaProjects/jrelifix/SusFiles/PerfectFL/Bears-42.txt \
+-mode jmutrepair \
+-srcjavafolder /src \
+-srctestfolder /test \
+-binjavafolder /target/classes \
+-bintestfolder /target/test-classes \
+-stopfirst true
--- regression_evaluation/openpnp-openpnp-110833060.sh
+++ regression_evaluation/openpnp-openpnp-110833060.sh
@@ -1,7 +1,7 @@
 java -cp astor.jar fr.inria.main.evolution.AstorMain \
--location /home/cuong/IdeaProjects/apr-repo/regression-bugs \
--dependencies /home/cuong/IdeaProjects/apr-repo/regression-bugs/target/dependency \
--faultfile /home/cuong/IdeaProjects/jrelifix/SusFiles/PerfectFL/openpnp-openpnp-110833060.txt \
+-location /Users/cuong/IdeaProjects/jrelifix/BugsDataset \
+-dependencies /Users/cuong/IdeaProjects/jrelifix/BugsDataset/target/dependency \
+-faultfile /Users/cuong/IdeaProjects/jrelifix/SusFiles/PerfectFL/openpnp-openpnp-110833060.txt \
 -mode jmutrepair \
 -srcjavafolder /src/main/java \
 -srctestfolder /src/test/java \
